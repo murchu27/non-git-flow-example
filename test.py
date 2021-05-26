3f5fa2541970c2115932e98bae0601c1ee85e45e
Michael Murphy <m.b.murphy15@gmail.com>
2021-05-26 10:02:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def main():
     print("This first one needed fixing")
     print("Here are some changes")
-    print("Even more changes")
+    print("All new changes")
     print("This one's ready now too")
